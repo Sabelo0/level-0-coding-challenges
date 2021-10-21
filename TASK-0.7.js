a3b55283944195da2fed8b4ebbf0fdcd42cc26fe
Sabelo0 <mtimkhulusabelo0@gmail.com>
2021-10-21 14:44:27 +0200

    wrote code for Task 0.7

---
+++ TASK-0.7.js
@@ -0,0 +1,10 @@
+function celciusToFahrenheit(celcius){
+    return (celcius * 9/5) + 32 + " Fahrenheit"
+}  
+
+function fahrenheitToCelcius(fahrenheit){
+    return (fahrenheit - 32) * 5/9 + " Celcius"
+}
+
+celciusToFahrenheit(37)
+fahrenheitToCelcius(98.6)
